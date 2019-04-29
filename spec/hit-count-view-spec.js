705e9cb25231ecea80723e452a9cf89f5424b286
Liang Kong <lkon0010@uni.sydney.edu.au>
2019-04-29 20:22:06 +1000

    Initial commit

---
+++ spec/hit-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HitCountView from '../lib/hit-count-view';
+
+describe('HitCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
